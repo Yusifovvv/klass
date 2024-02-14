7e7d9b620d1ca2a278af18ba45d1763e9009aab8
Yusifovvv <157576639+Yusifovvv@users.noreply.github.com>
2024-02-14 11:53:34 +0300

    Практическое занятие 14.02.2024.py

---
+++ 2main.py
@@ -0,0 +1,67 @@
+#coding:utf-8
+
+#Задача #1
+a=int(input())
+if a > 0:
+    print("Число больше нуля")
+elif a < 0:
+    print("Число меньше нуля")
+    
+#Задача 2
+a=int(input())
+if a > 0:
+     print("1")
+else:
+    print("-1")
+    
+#Задача 3
+h=input ("Пойдёшь чилить? (Да, Нет, Не знаю)")
+if h == "Да":
+    print("Погнали")
+elif h=="Нет":
+    print ("Не знаю")
+else:
+    print("Кринж")
+    
+#Задача 4
+a=int(input())
+k=int(input())
+if a < b:
+    c = a-k
+elif a < k:
+    c = a+k
+else:
+    c = a
+print(c)
+
+#Задача 5
+p=int(input()) ("Введите любые числа  от 11 до 23")
+if p==11:
+    print("11 - это барабанные палочки")
+elif p==12:
+     print("12 апреля")
+else:
+    print("Ты ввёл 13 наверное")
+if p==14:
+    print("14")
+elif p==15:
+    print("Я вчера съел 15 бургеров")
+else:
+    print("Да я всё видел, ты ввёл 16")
+if p==17:
+    print("17 пакетиков")
+elif p==18:
+    print("18+")
+else: 
+    print("Ты чё, тебе же 19")
+if p==20:
+    print("19+1")
+elif p==21:
+    print("22 номер ты был")
+else:
+    print("Ладно 23")
+    
+
+
+
+
